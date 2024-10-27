eb4f2ee30cc3b84aea1854bf613b563005434252
Greg Hicks <greghicks@iinet.net.au>
2024-10-27 23:20:38 +1000

    added capacitor for mobile

---
+++ front/honey-can-you-help-me-with-this/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'au.org.casualengineer',
+  appName: 'honey-can-you-help-me-with-this',
+  webDir: 'dist'
+};
+
+export default config;
---
+++ front/honey-can-you-help-me-with-this/src/app/report-abuse/report-abuse.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-report-abuse',
+  standalone: true,
+  imports: [],
+  templateUrl: './report-abuse.component.html',
+  styleUrl: './report-abuse.component.css'
+})
+export class ReportAbuseComponent {
+
+}
