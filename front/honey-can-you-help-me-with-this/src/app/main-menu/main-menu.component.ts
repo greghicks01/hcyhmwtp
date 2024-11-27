7d4d01456c874010c832d80c91cae3ee0c85701a
Greg Hicks <greghicks@iinet.net.au>
2024-11-28 07:43:00 +1000

    minor

--- front/honey-can-you-help-me-with-this/src/app/main-menu/main-menu.component.ts
+++ front/honey-can-you-help-me-with-this/src/app/main-menu/main-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
@@ -9,4 +10,23 @@ import { CommonModule } from '@angular/common';
   templateUrl: './main-menu.component.html',
   styleUrls: ['./main-menu.component.css']
 })
-export class MainMenuComponent {}
+export class MainMenuComponent {
+  parametersForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.parametersForm = this.fb.group({
+      username: ['', Validators.required],
+      notifications: ['', Validators.required]
+    });
+  }
+
+  onSubmitParameters() {
+    if (this.parametersForm.valid) {
+      const parameters = this.parametersForm.value;
+      console.log('Parameters saved:', parameters);
+      // Here you can add code to save the parameters, e.g., send them to a server
+    } else {
+      console.log('Please fill in all fields.');
+    }
+  }
+}
